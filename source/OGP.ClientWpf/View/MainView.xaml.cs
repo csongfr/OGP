af5ecd4b2992481b233f118fe9d3b6eff268ae7a
nla <nlafourcade@sopragroup.com>
2012-02-01 14:38:19 +0100

    Intégration du nouveau ribbon.

--- source/OGP.ClientWpf/View/MainView.xaml.cs
+++ source/OGP.ClientWpf/View/MainView.xaml.cs
@@ -11,17 +11,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Fluent;
+using OGP.ClientWpf.ViewModel;
 
-namespace OGP.ClientWpf
+namespace OGP.ClientWpf.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : RibbonWindow
     {
-        public MainWindow()
+        public MainView()
         {
-            InitializeComponent();
+            InitializeComponent();         
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ source/OGP.ClientWpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OGP.ClientWpf.Comands;
+using System.Windows.Input;
+using System.ComponentModel;
+
+
+
+
+namespace OGP.ClientWpf.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {                              
+        #region Events
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Raise PropertyChanged event
+        void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region Membres privés       
+
+        private RelayCommand exitCommand;
+
+        #endregion
+                             
+        #region Commands
+
+        #region Exit
+
+        /// <summary>
+        /// Exit from the application
+        /// </summary>
+        public ICommand ExitCommand
+        {
+            get
+            {
+                if (exitCommand == null)
+                {
+                    exitCommand = new RelayCommand(x => System.Windows.Application.Current.Shutdown());
+                }
+                return exitCommand;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constructeur
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainViewModel() {
+        
+        }
+
+        #endregion
+    }
+}
