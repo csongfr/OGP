e9e60f50b6e12fe72d098ef734c005354c00eaa1
nla <nlafourcade@sopragroup.com>
2012-02-07 10:30:02 +0100

    Mise à jour de la solution.

--- source/OGP.ClientWpf/ViewModel/MainViewModel.cs
+++ source/OGP.ClientWpf/ViewModel/MainViewModel.cs
@@ -8,58 +8,92 @@ using System.ComponentModel.Composition.Hosting;
 using System.ComponentModel.Composition;
 using AvalonDock;
 using System.IO;
+using OGP.ClientWpf.View;
 using System.Windows;
-
+using Utils.Observable;
 
 
 
 namespace OGP.ClientWpf.ViewModel
 {
-    public class MainViewModel : INotifyPropertyChanged
-    {                              
-        #region Events
-
-        /// <summary>
-        /// Occurs when a property value changes.
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // Raise PropertyChanged event
-        void RaisePropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
-        #endregion
-
-        #region Membres privés       
+    public class MainViewModel : ViewModelBase
+    {                                 
+        #region Membres privés
 
         private RelayCommand exitCommand;
 
         private CompositionContainer _container;
 
+        /// <summary>
+        /// Stocke le plugin actif.
+        /// </summary>
+        private String pluginActif;
+
+        /// <summary>
+        /// Stocke la liste des plugins actifs.
+        /// </summary>
+        private List<DocumentContent> listeDocuments;
         [ImportMany]
-        private IEnumerable<Lazy<DocumentContent, IDocumentData>> Plugins
+        private IEnumerable<Lazy<DocumentContent, IDocumentData>> Plugin
         {
             get;
             set;
         }
 
+        #endregion
+
+        #region listes plugins
+
+        public List<string> ListesPlugins
+        {
+            get;
+            set;
+        }
         #endregion
 
         #region Membres public
 
-        public List<DocumentContent> Documents
+        /// <summary>
+        /// Cinch : INPC Helper
+        /// </summary>
+        private static System.ComponentModel.PropertyChangedEventArgs listeDocumentsChangeArgs = Utils.Observable.ObservableHelper.CreateArgs<MainViewModel>(x => x.ListeDocuments);
+
+        public List<DocumentContent> ListeDocuments
         {
-            get;
-            set;
+            get
+            {
+                return listeDocuments;
+            }
+            set
+            {
+                this.listeDocuments = value;
+            }
+        }
+
+        /// <summary>
+        /// Cinch : INPC Helper
+        /// </summary>
+        private static System.ComponentModel.PropertyChangedEventArgs pluginActifChangeArgs = Utils.Observable.ObservableHelper.CreateArgs<MainViewModel>(x => x.PluginActif);
+
+        /// <summary>
+        /// Gets ou Sets du plugin actif.
+        /// </summary>
+        public String PluginActif
+        {
+            get
+            {
+                return pluginActif;
+            }
+            set
+            {
+                this.pluginActif = value;
+                ChargerPlugins();
+
+            }
         }
 
         #endregion
-                             
+
         #region Commands
 
         #region Exit
@@ -90,13 +124,27 @@ namespace OGP.ClientWpf.ViewModel
         /// </summary>
         public MainViewModel()
         {
-            this.Documents = new List<DocumentContent>();
+            this.ListeDocuments = new List<DocumentContent>();
+            this.ListesPlugins = new List<string>();
             ChargerPlugins();
+            Listeplugins();
         }
 
         #endregion
 
         #region Méthodes privées
+
+        private void Listeplugins()
+        {
+            foreach (var plugin in this.Plugin)
+            {
+                if (plugin.Value != null)
+                {
+                    this.ListesPlugins.Add(plugin.Metadata.Title);
+                }
+            }
+        }
+
 
         private void ChargerPlugins()
         {
@@ -106,12 +154,12 @@ namespace OGP.ClientWpf.ViewModel
                 catalog.Catalogs.Add(new DirectoryCatalog(@"..\..\Ressources\Plugins"));
                 _container = new CompositionContainer(catalog);
                 this._container.ComposeParts(this);
-
-                foreach (var plugin in this.Plugins)
+                foreach (var plugin in this.Plugin)
                 {
                     if (plugin.Value != null)
                     {
-                        this.Documents.Add(plugin.Value);
+                        if (plugin.Metadata.Title.Equals(pluginActif))
+                            this.ListeDocuments.Add(plugin.Value);
                     }
                 }
             }
@@ -127,8 +175,11 @@ namespace OGP.ClientWpf.ViewModel
             {
                 Console.Error.WriteLine(ex.Message);
             }
+
         }
 
         #endregion
     }
 }
+
+
---
+++ source/OGP.ClientWpf/ViewModel/ViewModelBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace OGP.ClientWpf.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Events
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>  
+        /// Notify using pre-made PropertyChangedEventArgs
+        /// </summary>
+        /// <param name="propertyName">Propertiy notified.</param>
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// Notify using pre-made PropertyChangedEventArgs
+        /// </summary>
+        /// <param name="args">Propertiy notified.</param>
+        protected void NotifyPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, args);
+            }
+        }
+
+        #endregion
+    }
+
+}
