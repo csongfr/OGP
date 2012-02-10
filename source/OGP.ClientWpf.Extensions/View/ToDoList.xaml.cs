1a6e1c6cb29701cb64e41f4da94ae96cc97aff9a
mBortolin <mbortolin@sopragroup.com>
2012-02-10 09:29:09 +0100

    MonDocument renomer en ToDoList

---
+++ source/OGP.ClientWpf.Extensions/View/ToDoList.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+using AvalonDock;
+
+namespace OGP.ClientWpf.Extensions
+{
+    /// <summary>
+    /// Logique d'interaction pour MonDocument.xaml
+    /// </summary>
+    [Export(typeof(DocumentContent))]
+    [ExportMetadata("Title", "ToDoList")]
+    public partial class ToDoList : DocumentContent
+    {
+        /// <summary>
+        /// Constructeur de la TODoList
+        /// </summary>
+        public ToDoList()
+        {
+            InitializeComponent();
+            this.DataContext = new ToDoListViewModel();
+        }
+    }
+}
