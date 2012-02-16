cb46d32dfee00c426145da457e7b35f09656c820
mBortolin <mbortolin@sopragroup.com>
2012-02-16 11:22:01 +0100

    Ajout du MenuViewModel

--- source/Todolist.Client/Properties/AssemblyInfo.cs
+++ source/Todolist.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Plugin.Todolist")]
+[assembly: AssemblyTitle("Todolist.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Plugin.Todolist")]
+[assembly: AssemblyProduct("Todolist.Client")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
