8ca4330209075eae0176e734cc9beadb5b37336c
Vivien Ruiz <ruiz.vivien@gmail.com>
2012-03-06 14:08:21 +0100

    Ajout de Utils.

---
+++ source/Utils/UtilsApplicationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Utils
+{
+    [Serializable]
+    public class UtilsApplicationException : ApplicationException
+    {
+        public UtilsApplicationException() { }
+        public UtilsApplicationException(string message) : base(message) { }
+        public UtilsApplicationException(string message, Exception inner) : base(message, inner) { }
+        protected UtilsApplicationException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
