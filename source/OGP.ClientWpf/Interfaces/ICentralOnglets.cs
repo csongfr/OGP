9e0abbf84eb38acce5b5aa721ce04320a8387e02
mbortolin <mbortolin@sopragroup.com>
2012-06-05 12:00:04 +0200

    Fichiers

---
+++ source/OGP.ClientWpf/Interfaces/ICentralOnglets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OGP.ClientWpf.ViewModel;
+
+namespace OGP.ClientWpf.Interfaces
+{
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public interface ICentralOnglets
+    {
+        /// <summary>
+        /// Met au premier plan l'onglet fourni. Si celui-ci n'est pas dans la liste des onglets, il est ajouté.
+        /// </summary>
+        /// <param name="workspace">Onglet à afficher.</param>
+         void NouvelOnglet(string Nom);
+    }
+}
