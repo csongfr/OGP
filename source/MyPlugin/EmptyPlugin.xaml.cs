eeb255c7ce5c1d5f68f504e8442767bd76bed931
csong <csongfr@gmail.com>
2012-11-07 18:25:39 +0100

    plugin vide

---
+++ source/MyPlugin/EmptyPlugin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Fluent;
+using OGP.Plugin.Interfaces;
+
+
+namespace MyPlugin
+{
+    /// <summary>
+    /// Interaction logic for EmptyPlugin.xaml
+    /// </summary>
+    [Export(typeof(IOgpMenu))]
+    public partial class EmptyPlugin : RibbonTabItem, IOgpMenu
+    {
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        public EmptyPlugin()
+        {
+            InitializeComponent();
+            DataContext = new EmptyPluginViewModel();
+        }
+    }
+}
+
---
+++ source/MyPlugin/EmptyPluginViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using AvalonDock;
+using Cinch;
+using Fluent;
+
+namespace MyPlugin
+{
+    public class EmptyPluginViewModel : ViewModelBase
+    {
+        #region Commandes
+
+        /// <summary>
+        /// Stocke la commande qui fait le café.
+        /// </summary>
+        private SimpleCommand commandeOuvrirFichier;
+
+        /// <summary>
+        /// Gets ou sets la commande qui fait le café.
+        /// </summary>
+        public SimpleCommand CommandeOuvrirFichier
+        {
+            get
+            {
+                if (commandeOuvrirFichier == null)
+                {
+                    commandeOuvrirFichier = new SimpleCommand
+                    {
+                        ExecuteDelegate = delegate
+                        {
+                            NouvelOnglet();
+                        }
+                    };
+                }
+                return commandeOuvrirFichier;
+            }
+        }
+
+        #endregion
+
+        #region Onglets
+
+        /// <summary>
+        /// Nouvel onglets
+        /// </summary>
+        private void NouvelOnglet()
+        {
+        }
+
+        #endregion
+    }
+}
